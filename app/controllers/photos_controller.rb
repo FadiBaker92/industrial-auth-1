1b30dc4b8741b6834cb200caaec48fa9e48c27b2
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-08-17 16:46:42 +0000

    photo policy done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
+  before_action :ensure_user_is_authorized, only: [:show]
 
   # GET /photos or /photos.json
   def index
@@ -69,6 +70,11 @@ class PhotosController < ApplicationController
       if current_user != @photo.owner
         redirect_back fallback_location: root_url, alert: "You are not authorized for that"
       end
+    end
+
+    def ensure_user_is_authorized
+      if !PhotoPolicy.new(current_user, @photo).show?
+        raise Pundit::NotAuthorizedError, "not allowed"      end
     end
 
     # Only allow a list of trusted parameters through.
